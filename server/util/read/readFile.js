0807c7ef40f6309c1c5c3b46d22e7e70ecc44f6b
zertu <290055513@qq.com>
2017-04-03 21:04:16 +0800

    读取文件

--- server/util/read/readFile.js
+++ server/util/read/readFile.js
@@ -1,4 +1,24 @@
-const fs =require('fs')
-module.exports=function (Filename) { 
-    
- }
+const fs = require('fs')
+module.exports = function (Filename, option) {
+    return new Promise((resolve, reject) => {
+        let defaultopintion = {
+            encoding: 'utf-8',
+            flag: 'r'
+        }
+        if (option) {
+            if (typeof(option) === 'string') {
+                defaultopintion.encoding = option
+            } else if (Object.prototype.toString.call(option) === "[object Object]") {
+                for (let i in option) {
+                    defaultopintion[i] = option[i]
+                }
+            }
+        }
+        fs.readFile(Filename, defaultopintion, (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(data)
+        })
+    })
+}
